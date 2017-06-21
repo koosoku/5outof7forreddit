a9ccb2cc6c0152d3f48d70926f71872214f962b9
Kyle Kung <kkung065@uottawa.ca>
2017-06-21 16:45:58 -0400

    Implemented fragments with a list view for future reddit posts

--- app/src/main/java/kyle/a57/a57forreddit/MainActivity.java
+++ app/src/main/java/kyle/a57/a57forreddit/MainActivity.java
@@ -2,12 +2,25 @@ package kyle.a57.a57forreddit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import kyle.a57.a57forreddit.dummy.DummyContent;
 
+
+public class MainActivity extends AppCompatActivity implements PostsFragment.OnListFragmentInteractionListener{
+
+    private static final int FRAGMENT_POST_ID = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, PostsFragment.newInstance(1)).commit();
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
