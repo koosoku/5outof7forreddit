5c0d2bbab1b52e02f0ec86701bd214dde0a3f6cf
Kyle Kung <kkung065@uottawa.ca>
2017-06-21 15:27:32 -0400

    Initial Commit

---
+++ app/src/main/java/kyle/a57/a57forreddit/MainActivity.java
@@ -0,0 +1,13 @@
+package kyle.a57.a57forreddit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
